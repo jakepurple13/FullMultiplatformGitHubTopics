e7f8005b4e2d5380b639f918bb1f3fdb52c7c370
Jacob Rein <jakerein@gmail.com>
2023-01-10 08:45:01 -0500

    Small cleanup

--- shared/src/androidMain/kotlin/com/programmersbox/shared/platform.kt
+++ shared/src/androidMain/kotlin/com/programmersbox/shared/platform.kt
@@ -1,16 +1,10 @@
 package com.programmersbox.shared
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.programmersbox.shared.screens.TopicDrawer
 import com.programmersbox.shared.utils.AppActions
 import com.programmersbox.shared.viewmodels.BaseTopicVM
@@ -51,15 +45,11 @@ internal actual fun TopicDrawerLocation(vm: BaseTopicVM) {
         }
     ) {
         Surface {
-            Column {
-                Spacer(Modifier.height(30.dp))
-                DismissibleNavigationDrawer(
-                    drawerContent = { DismissibleDrawerSheet { TopicDrawer(vm) } },
-                    drawerState = drawerState,
-                    modifier = Modifier.padding(vertical = 30.dp)
-                ) {
-                    App(vm)
-                }
+            DismissibleNavigationDrawer(
+                drawerContent = { DismissibleDrawerSheet { TopicDrawer(vm) } },
+                drawerState = drawerState,
+            ) {
+                App(vm)
             }
         }
     }
--- shared/src/jsMain/kotlin/com/programmersbox/shared/platform.kt
+++ shared/src/jsMain/kotlin/com/programmersbox/shared/platform.kt
@@ -1,15 +1,9 @@
 package com.programmersbox.shared
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.programmersbox.shared.screens.TopicDrawer
 import com.programmersbox.shared.utils.AppActions
 import com.programmersbox.shared.viewmodels.BaseTopicVM
@@ -37,14 +31,10 @@ internal actual fun TopicDrawerLocation(vm: BaseTopicVM) {
         }
     ) {
         Surface {
-            Column {
-                Spacer(Modifier.height(30.dp))
-                PermanentNavigationDrawer(
-                    drawerContent = { PermanentDrawerSheet { TopicDrawer(vm) } },
-                    modifier = Modifier.padding(vertical = 30.dp)
-                ) {
-                    App(vm)
-                }
+            PermanentNavigationDrawer(
+                drawerContent = { PermanentDrawerSheet { TopicDrawer(vm) } },
+            ) {
+                App(vm)
             }
         }
     }
